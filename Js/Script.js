4e40bd30dfe7326d598b313d469e44a5bd7a9645
Sassarrao <jp.sdc006@gmail.com>
2025-06-04 22:44:38 -0300

    Implementação das questões do quiz em javascript

---
+++ Js/Script.js
@@ -0,0 +1,92 @@
+const questoes = [
+    {
+        questao: 'Qual é o principal problema que o FlowSafe busca resolver?',
+        respostas: [
+            { id: 1, text: 'Falta de energia elétrica durante enchentes', correct: false},
+            { id: 2, text: 'Falta de comunicação entre autoridades', correct: false},
+            { id: 3, text: 'Falta de informações seguras e em tempo real sobre abrigos', correct: true},
+            { id: 4, text: 'Poluição causada por enchentes', correct: false}
+        ]
+    },
+    {
+        questao: 'Qual jogo foi criado com Python e Pygame para conscientizar sobre áreas de risco?',
+        respostas: [
+            { id: 1, text: 'Jogo da Memória', correct: false},
+            { id: 2, text: 'Campo Minado', correct: true},
+            { id: 3, text: 'Tetris', correct: false},
+            { id: 4, text: 'Pong', correct: false}
+        ]
+    },
+    {
+        questao: 'Qual tecnologia física é usada para monitorar bueiros e emitir alertas?',
+        respostas: [
+            { id: 1, text: 'Impressora 3D', correct: false},
+            { id: 2, text: 'Raspberry Pi com display', correct: false},
+            { id: 3, text: 'Arduino com sensores, LEDs e buzzer', correct: true},
+            { id: 4, text: 'Drone de inspeção', correct: false}
+        ]
+    },
+    {
+        questao: 'Qual é o principal objetivo da plataforma durante emergências?',
+        respostas: [
+            { id: 1, text: 'Fornecer alertas e orientações em tempo real', correct: true},
+            { id: 2, text: 'Entretenimento educativo', correct: false},
+            { id: 3, text: 'Coleta de doações online', correct: false},
+            { id: 4, text: 'Criar jogos sobre enchentes', correct: false}
+        ]
+    },
+    {
+        questao: 'Como o FlowSafe contribui para a mobilidade em emergências?',
+        respostas: [
+            { id: 1, text: 'Evitando o uso de transporte público', correct: false},
+            { id: 2, text: 'Informando sobre bloqueios policiais', correct: false},
+            { id: 3, text: 'Enviando motoristas para resgates', correct: false},
+            { id: 4, text: 'Indicando rotas seguras e abrigos atualizados', correct: true},
+        ]
+    },
+    {
+        questao: 'Quem são os principais beneficiados pela plataforma?',
+        respostas: [
+            { id: 1, text: 'Estudantes de tecnologia', correct: false},
+            { id: 2, text: 'Trabalhadores do setor privado', correct: false},
+            { id: 3, text: 'Moradores de áreas de risco, voluntários e ONGs', correct: true},
+            { id: 4, text: 'Desenvolvedores de jogos', correct: false}
+        ]
+    },
+    {
+        questao: 'Como a plataforma reduz o número de pessoas desabrigadas?',
+        respostas: [
+            { id: 1, text: 'Distribuindo barracas infláveis', correct: false},
+            { id: 2, text: 'Direcionando rapidamente desabrigados a locais seguros', correct: true},
+            { id: 3, text: 'Enviando notificações para que todos fiquem em casa', correct: false},
+            { id: 4, text: 'Oferecendo cupons de transporte', correct: false}
+        ]
+    },
+    {
+        questao: 'Quais tecnologias são usadas para desenvolver a interface do FlowSafe?',
+        respostas: [
+            { id: 1, text: 'JavaScript e React Native', correct: false},
+            { id: 2, text: 'Bootstrap e PHP', correct: false},
+            { id: 3, text: 'HTML e CSS para landing page com blog e gráficos', correct: true},
+            { id: 4, text: 'Python e Flask', correct: false}
+        ]
+    },
+    {
+        questao: 'Qual é um dos benefícios diretos oferecidos pela plataforma?',
+        respostas: [
+            { id: 1, text: 'Alerta de queda de temperatura', correct: false},
+            { id: 2, text: 'Status em tempo real dos abrigos', correct: true},
+            { id: 3, text: 'Monitoramento do clima para turistas', correct: false},
+            { id: 4, text: 'Compartilhamento de fotos da enchente', correct: false}
+        ]
+    },
+    {
+        questao: 'Como o FlowSafe ajuda a otimizar a ocupação dos abrigos?',
+        respostas: [
+            { id: 1, text: 'Organizando dados em tempo real para melhor distribuição de pessoas', correct: true},
+            { id: 2, text: 'Distribuindo senha digital por SMS', correct: false},
+            { id: 3, text: 'Informando número de voluntários', correct: false},
+            { id: 4, text: 'Bloqueando entradas após atingir 50% de ocupação', correct: false}
+        ]
+    },
+]
